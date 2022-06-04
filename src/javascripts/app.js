0aaab5bb59573cac7c5f1519f13ad697d12d6cd0
Michal Terlecki <michterlecki@gmail.com>
2022-06-04 09:38:45 +0200

    cookie modal added

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -13,5 +13,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     onDomReady();
 
+    const cookieModal = document.querySelector('#cookie-modal');
+    const modal = new Modal();
+    modal.openModal(cookieModal)
+
 }, false);
 
